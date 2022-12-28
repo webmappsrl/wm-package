8824844ea1b7ddff576f57293e4a369c5c78dc4e
mbaroncini <mbaroncini@users.noreply.github.com>
2022-12-28 13:46:35 +0000

    Fix styling

--- routes/api.php
+++ routes/api.php
@@ -5,26 +5,22 @@ use Illuminate\Support\Facades\Route;
 use Wm\WmPackage\Http\Controllers\AuthController;
 
 Route::prefix('api')->middleware('api')->group(function () {
+    // Public routes
 
-  // Public routes
+    Route::post('login', [AuthController::class, 'login']);
 
-  Route::post('login', [AuthController::class, 'login']);
+    // Protected routes
 
-  // Protected routes
+    /**
+     * Only users with special token ability can register users
+     */
+    Route::post('register', [AuthController::class, 'register'])
+      ->middleware(['auth:sanctum', 'abilities:create-users']);
 
-  /**
-   * Only users with special token ability can register users
-   */
-  Route::post('register', [AuthController::class, 'register'])
-    ->middleware(['auth:sanctum', 'abilities:create-users']);
-
-  /**
-   *
-   */
-  Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('user', function (Request $request) {
-      return $request->user();
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::get('user', function (Request $request) {
+            return $request->user();
+        });
+        Route::post('logout', [AuthController::class, 'logout']);
     });
-    Route::post('logout', [AuthController::class, 'logout']);
-  });
 });
--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -3,75 +3,74 @@
 namespace Wm\WmPackage\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-
 class AuthController extends Controller
 {
-  use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-  public function register(Request $request)
-  {
-    $fields = $request->validate([
-      'name' => 'required|string',
-      'email' => 'required|string|unique:users,email',
-      'password' => 'required|string|confirmed'
-    ]);
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
 
-    $user = User::create([
-      'name' => $fields['name'],
-      'email' => $fields['email'],
-      'password' => bcrypt($fields['password']),
-    ]);
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
 
-    $token = $user->createToken($request->ip())->plainTextToken;
+        $token = $user->createToken($request->ip())->plainTextToken;
 
-    $response = [
-      'user' => $user,
-      'token' => $token,
-    ];
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
 
-    return response($response, 201);
-  }
-
-  public function login(Request $request)
-  {
-    $fields = $request->validate([
-      'email' => 'required|string',
-      'password' => 'required|string'
-    ]);
-
-    // Check email
-    $user = User::where('email', $fields['email'])->first();
-
-    // Check password
-    if (!$user || !Hash::check($fields['password'], $user->password)) {
-      return response([
-        'message' => 'Bad creds'
-      ], 401);
+        return response($response, 201);
     }
 
-    $token = $user->createToken($request->ip())->plainTextToken;
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
 
-    $response = [
-      'user' => $user,
-      'token' => $token,
-    ];
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
 
-    return response($response, 201);
-  }
+        // Check password
+        if (! $user || ! Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds',
+            ], 401);
+        }
 
+        $token = $user->createToken($request->ip())->plainTextToken;
 
-  public function logout(Request $request)
-  {
-    $request->user()->currentAccessToken()->delete();
-    return [
-      'message' => 'Logged out',
-    ];
-  }
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return [
+            'message' => 'Logged out',
+        ];
+    }
 }
