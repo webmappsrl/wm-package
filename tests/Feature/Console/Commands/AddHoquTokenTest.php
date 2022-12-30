0f25b9808301aed6620ddad19451ebf67204e1e2
marco <m.baroncini@cyberway.cloud>
2022-12-30 09:28:26 +0100

    Add hoqu command test

---
+++ tests/Feature/Console/Commands/AddHoquTokenTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Console\Commands;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Wm\WmPackage\Tests\TestCase;
+
+class AddHoquTokenTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_hoqu_add_toke_command_existence()
+    {
+        $testToken = 'blablabla';
+        $this->artisan("hoqu:add-token", ['token' => $testToken])->assertSuccessful();
+    }
+}
