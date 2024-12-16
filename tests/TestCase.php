33cabb3013a93dfb4a4b60effbea5d3b6107961b
marco <m.baroncini@cyberway.cloud>
2024-12-16 09:36:12 +0100

    test: remove some unused stuff

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,22 +16,9 @@ class TestCase extends Orchestra
         ];
     }
 
-    protected function defineRoutes($router)
-    {
-        require __DIR__.'/../routes/web.php';
-    }
-
     protected function getEnvironmentSetUp($app)
     {
         // set app key for testing routes
-        $app['config']->set('app.key', 'base64:'.base64_encode(random_bytes(32)));
-
-        // set public filesystem for testing
-        $app['config']->set('filesystems.disks.public', [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-        ]);
+        $app['config']->set('app.key', 'base64:' . base64_encode(random_bytes(32)));
     }
 }
