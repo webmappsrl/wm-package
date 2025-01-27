a924aaf41efdf502ce294ffa0372356c2fc75ad6
marco <m.baroncini@cyberway.cloud>
2025-01-27 16:27:51 +0100

    feat: refactored OutSource models oc:4667

---
+++ src/Models/OutSourcePoi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wm\WmPackage\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class OutSourcePoi extends OutSourceFeature
+{
+    use HasFactory;
+
+    private $type = 'poi';
+
+    public function ecPois(): HasMany
+    {
+        return $this->hasMany(EcPoi::class);
+    }
+}
