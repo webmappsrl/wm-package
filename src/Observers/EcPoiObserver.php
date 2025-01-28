02b7a01b85c4f9e2e18fd0e4498ea51cfb35c499
marco <m.baroncini@cyberway.cloud>
2025-01-28 09:27:27 +0100

    feat: updated EcPoiObserver oc:4667

--- src/Observers/EcPoiObserver.php
+++ src/Observers/EcPoiObserver.php
@@ -2,9 +2,10 @@
 
 namespace Wm\WmPackage\Observers;
 
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Wm\WmPackage\Models\EcMedia;
 use Wm\WmPackage\Models\EcPoi;
+use Wm\WmPackage\Services\Models\UserService;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Wm\WmPackage\Services\Models\EcPoiService;
 
 class EcPoiObserver extends AbstractObserver
 {
@@ -18,5 +19,19 @@ class EcPoiObserver extends AbstractObserver
         if ($ecPoi->ecTracks()->exists()) {
             throw new HttpException(500, 'Cannot delete this POI because it is linked to one or more tracks.');
         }
+    }
+
+    /**
+     * Handle the EcMedia "saved" event.
+     *
+     * @return void
+     */
+    public function saved(EcPoi $ecPoi)
+    {
+        if (! $ecPoi->skip_geomixer_tech && ! empty($ecPoi->geometry)) {
+            EcPoiService::make()->updateDataChain($ecPoi);
+        }
+
+        UserService::make()->assigUserSkuAndAppIdIfNeeded($ecPoi->user, $ecPoi->sku, $ecPoi->app_id);
     }
 }
