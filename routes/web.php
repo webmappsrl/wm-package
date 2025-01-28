3101f969c27b75d4f590024e45d3074127fbf13c
marco <m.baroncini@cyberway.cloud>
2025-01-28 11:37:49 +0100

    feat: refactor controller oc:4667

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Wm\WmPackage\Http\Controllers\RankingController;
 use Wm\WmPackage\Http\Controllers\ExportDownloadController;
 
 Route::get('/download-export/{fileName}', [ExportDownloadController::class, 'download'])
     ->name('download.export')
     ->middleware(['web', 'signed']);
+
+
+Route::get('/top-ten/{app}', [RankingController::class, 'showTopTen'])->name('top-ten');
+Route::get('/user-ranking/{app}/{user}', [RankingController::class, 'showUserRanking'])->name('user-ranking');
+
+
+// TODO: Use A middleware to switch the language
+// Route::get('language/{locale}', function ($locale) {
+//     app()->setLocale($locale);
+//     session()->put('locale', $locale);
+
+//     return redirect()->back();
+// });
