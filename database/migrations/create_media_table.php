b75f46093e0df5c9b9a65995fbcafb29e18916b7
marco <m.baroncini@cyberway.cloud>
2025-02-03 14:51:04 +0100

    feat: initial refactor of media oc:4667

--- database/migrations/create_media_table.php
+++ database/migrations/create_media_table.php
@@ -28,6 +28,8 @@ return new class extends Migration
             $table->unsignedInteger('order_column')->nullable()->index();
 
             $table->nullableTimestamps();
+
+            $table->spatialIndex('geometry');
         });
     }
 };
