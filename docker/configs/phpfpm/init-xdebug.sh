ec32d29afd0054186d071c97cb8f8d92dd3861ea
FalconClaws059 <valerio.bertolone1998@gmail.com>
2025-02-06 13:53:27 +0100

    fix: Fixed wrong testing condition oc:4883

--- docker/configs/phpfpm/init-xdebug.sh
+++ docker/configs/phpfpm/init-xdebug.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source .env
-containerName="php81_$APP_NAME"
+containerName="php_wm_package"
 docker exec -it -u 0 $containerName pecl install xdebug
 
 docker cp ./docker/configs/phpfpm/xdebug.ini $containerName:/usr/local/etc/php/conf.d/.
