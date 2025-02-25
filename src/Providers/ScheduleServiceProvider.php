d86016ba1768d632d97d3b7452c56791e90251f8
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-02-25 15:26:42 +0100

    chore: Add ScheduleServiceProvider for automated backup tasks

---
+++ src/Providers/ScheduleServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wm\WmPackage\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\App;
+
+class ScheduleServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+
+            if (App::environment('production')) {
+
+                $schedule->command('wm-backup:run --only-db')
+                    ->daily()
+                    ->at('20:00');
+
+                $schedule->command('wm-backup:run --only-files')
+                    ->fridays()
+                    ->at('18:00');
+
+
+                $schedule->command('backup:clean')
+                    ->sundays()
+                    ->at('03:00');
+            }
+        });
+    }
+}
