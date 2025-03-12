3d50af9abe5e29a0013c411dd08bb90331f76d25
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-03-12 14:43:53 +0100

    refactor: Enhance BaseImportJob with additional data handling and improved method definitions for model and table retrieval

--- src/Jobs/Import/BaseImportJob.php
+++ src/Jobs/Import/BaseImportJob.php
@@ -4,11 +4,12 @@ namespace Wm\WmPackage\Jobs\Import;
 
 use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 use Wm\WmPackage\Services\Import\GeohubImportService;
 
 abstract class BaseImportJob implements ShouldQueue
@@ -20,6 +21,8 @@ abstract class BaseImportJob implements ShouldQueue
      */
     protected int $entityId;
 
+    protected array $data;
+
     /**
      * The Geohub import service
      */
@@ -28,9 +31,10 @@ abstract class BaseImportJob implements ShouldQueue
     /**
      * Create a new job instance.
      */
-    public function __construct(int $entityId)
+    public function __construct(int $entityId, array $data = [])
     {
         $this->entityId = $entityId;
+        $this->data = $data;
         $this->onQueue('geohub-import');
     }
 
@@ -40,43 +44,63 @@ abstract class BaseImportJob implements ShouldQueue
     public function handle(GeohubImportService $importService): void
     {
         $this->geohubImportService = $importService;
-
         $logger = Log::channel(config('wm-geohub-import.import_log_channel', 'wm-package-failed-jobs'));
         $modelName = $this->getModelName();
-        $tableName = $this->getTableName();
 
         try {
-            $data = $this->geohubImportService->fetchData($this->entityId, $tableName);
+            $data = $this->geohubImportService->fetchData($this->entityId, $this->getTableName());
             $transformedData = $this->transformData($data);
 
-            $this->geohubImportService->importData($transformedData, $modelName, $this->entityId);
+            $model = $this->geohubImportService->importData($transformedData, $modelName, $this->entityId);
 
-            $this->processDependencies($transformedData);
+            //using $data instead of $transformedData for referenced keys in geohub database
+            $this->processDependencies($data, $model);
 
-            $logger->info("Completed import of {$modelName} with ID {$this->entityId}", [
-                'result' => $result,
-            ]);
+            $logger->info("Completed import of {$modelName} with ID {$this->entityId}");
         } catch (\Exception $e) {
             $logger->error("Failed to import {$modelName} with ID {$this->entityId}: {$e->getMessage()}", [
                 'exception' => $e,
             ]);
+            throw $e;
         }
     }
 
     /**
      * Get the model name for this job.
      */
-    abstract protected function getModelName(): string;
+    protected function getModelName(): string
+    {
+        return config('wm-geohub-import.import_mapping.' . $this->getModelKey() . '.namespace');
+    }
 
     /**
      * Get the table name for this job.
      */
-    abstract protected function getTableName(): string;
+    protected function getTableName(): string
+    {
+        return (new ($this->getModelName()))->getTable();
+    }
 
     /**
      * Get the mapping configuration for this entity type.
      */
-    abstract protected function getMapping(): array;
+    protected function getMapping(): array
+    {
+        return config('wm-geohub-import.mappings.' . $this->getModelKey());
+    }
+
+    /**
+     * Get the relations for this entity type.
+     */
+    protected function getRelations(): array
+    {
+        return config('wm-geohub-import.import_mapping.' . $this->getModelKey() . '.relations');
+    }
+
+    /**
+     * Get the model key for this job.
+     */
+    abstract protected function getModelKey(): string;
 
     /**
      * Transform the data.
@@ -86,5 +110,5 @@ abstract class BaseImportJob implements ShouldQueue
     /**
      * Process dependencies if needed.
      */
-    abstract protected function processDependencies(array $transformedData): void;
+    abstract protected function processDependencies(array $transformedData, Model $model): void;
 }
