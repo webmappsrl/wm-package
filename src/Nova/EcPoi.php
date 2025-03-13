17a0d89fb9561e05ef27c4b9f9174b1a8124a60c
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-03-13 15:29:33 +0100

    feat: Integrate PointResourceTrait in EcPoi to enable map field

--- src/Nova/EcPoi.php
+++ src/Nova/EcPoi.php
@@ -4,15 +4,20 @@ namespace Wm\WmPackage\Nova;
 
 use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Wm\WmPackage\Nova\Traits\PointResourceTrait;
 
 class EcPoi extends AbstractEcResource
 {
+    use PointResourceTrait {
+        fields as protected fieldsTrait;
+    }
+
     public static $model = \Wm\WmPackage\Models\EcPoi::class;
 
     public function fields(NovaRequest $request): array
     {
         return [
-            ...parent::fields($request),
+            ...$this->fieldsTrait($request),
             BelongsToMany::make('EcTracks', 'ecTracks', EcTrack::class),
         ];
     }
