91382f64d7e04332c25c10ad6315716ca84fd7b2
marco <m.baroncini@cyberway.cloud>
2025-03-19 10:35:53 +0100

    fix: a problem with app relations on nova

--- src/Nova/Layer.php
+++ src/Nova/Layer.php
@@ -2,4 +2,24 @@
 
 namespace Wm\WmPackage\Nova;
 
-class Layer extends AbstractGeometryResource {}
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Code;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+
+class Layer extends AbstractGeometryResource
+{
+
+
+    public function fields(NovaRequest $request): array
+    {
+        return  [
+            ID::make()->sortable(),
+            Text::make('Name', 'name'),
+            BelongsTo::make('App', 'appOwner', App::class),
+            Code::make('Properties', $this->getPropertiesColumnName())->json(),
+        ];
+    }
+}
