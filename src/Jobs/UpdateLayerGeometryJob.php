695b4ddd5f6fbb9aceb9a03f68f080e122c75672
marco <m.baroncini@cyberway.cloud>
2025-03-24 11:55:09 +0100

    feat: make update layer geometry job unique

--- src/Jobs/UpdateLayerGeometryJob.php
+++ src/Jobs/UpdateLayerGeometryJob.php
@@ -3,16 +3,17 @@
 namespace Wm\WmPackage\Jobs;
 
 use Illuminate\Bus\Queueable;
+use Wm\WmPackage\Models\Layer;
+use Wm\WmPackage\Models\EcTrack;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Wm\WmPackage\Models\EcTrack;
-use Wm\WmPackage\Models\Layer;
-use Wm\WmPackage\Services\GeometryComputationService;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Wm\WmPackage\Services\Models\LayerService;
+use Wm\WmPackage\Services\GeometryComputationService;
 
-class UpdateLayerGeometryJob implements ShouldQueue
+class UpdateLayerGeometryJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -22,6 +23,15 @@ class UpdateLayerGeometryJob implements ShouldQueue
      * @return void
      */
     public function __construct(protected Layer $layer) {}
+
+
+    /**
+     * Get the unique ID for the job.
+     */
+    public function uniqueId(): string
+    {
+        return $this->layer->id;
+    }
 
     /**
      * Execute the job.
