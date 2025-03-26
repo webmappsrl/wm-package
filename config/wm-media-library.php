3a4651631bb7051bc25c88381383e543d236326d
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-03-26 15:30:35 +0100

    refactor: remove legacy media library configuration and integrate essential settings into wm-media-library.php

--- config/wm-media-library.php
+++ config/wm-media-library.php
@@ -5,4 +5,12 @@ return [
      * The fully qualified class name of the media model.
      */
     'media_model' => Wm\WmPackage\Models\Media::class,
+    /*
+     * The disk name to use for media storage.
+     */
+    'disk_name' => env('MEDIA_DISK', 'wmfe'),
+    /*
+     * The path generator to use for media storage.
+     */
+    'path_generator' => \Wm\WmPackage\Support\PathGenerator\WmfePathGenerator::class,
 ];
