25150a9ad0be2678f0fc3bc4091f81d583682793
marco <m.baroncini@cyberway.cloud>
2025-04-02 17:36:31 +0200

    fix: import of ectrack

--- src/Services/Models/LayerService.php
+++ src/Services/Models/LayerService.php
@@ -167,6 +167,10 @@ class LayerService extends BaseService
     public function updateLayerIdsPropertyOnLayeredFeature(GeometryModel $geometryModel, array $layerIds, bool $add)
     {
         $properties = $geometryModel->properties;
+
+        if (! isset($properties['layers']))
+            $properties['layers'] = [];
+
         if ($add) {
             $properties['layers'] = array_merge($properties['layers'], $layerIds);
         } else {
