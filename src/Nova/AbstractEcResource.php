50c470bc0a6d4f0680d2c723d271e3b47b06407a
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-04-07 14:27:15 +0200

    feat: add image field to AbstractEcResource and AbstractUgcResource for detail view

--- src/Nova/AbstractEcResource.php
+++ src/Nova/AbstractEcResource.php
@@ -3,10 +3,19 @@
 namespace Wm\WmPackage\Nova;
 
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
 use Wm\WmPackage\Nova\Filters\FeaturesByLayerFilter;
 
 abstract class AbstractEcResource extends AbstractGeometryResource
 {
+
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            ...parent::fields($request),
+            Images::make('Image', 'default')->onlyOnDetail(),
+        ];
+    }
     public function filters(NovaRequest $request): array
     {
         return [
--- src/Nova/AbstractUgcResource.php
+++ src/Nova/AbstractUgcResource.php
@@ -4,7 +4,10 @@ namespace Wm\WmPackage\Nova;
 
 use App\Nova\User;
 use Laravel\Nova\Fields\BelongsTo;
+use Wm\WmPackage\Nova\Actions\CopyUgc;
+use Wm\WmPackage\Nova\Actions\ExportTo;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
 
 abstract class AbstractUgcResource extends AbstractGeometryResource
 {
@@ -18,6 +21,7 @@ abstract class AbstractUgcResource extends AbstractGeometryResource
         return [
             ...parent::fields($request),
             BelongsTo::make('Author', 'author', User::class),
+            Images::make('Image', 'default')->onlyOnDetail(),
         ];
     }
 }
