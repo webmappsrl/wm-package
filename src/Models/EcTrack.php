b42ffc3f1868428112c538bf51e33c1729f3536b
mbaroncini <mbaroncini@users.noreply.github.com>
2025-04-07 08:10:50 +0000

    Fix styling

--- src/Models/EcTrack.php
+++ src/Models/EcTrack.php
@@ -55,8 +55,9 @@ class EcTrack extends MultiLineString implements LayerRelatedModel
     public function updateManualDataField($field, $value)
     {
         $properties = $this->properties;
-        if (! isset($properties['manual_data']))
+        if (! isset($properties['manual_data'])) {
             $properties['manual_data'] = [];
+        }
         $properties['manual_data'][$field] = $value;
         $this->properties = $properties;
     }
@@ -320,7 +321,7 @@ class EcTrack extends MultiLineString implements LayerRelatedModel
     {
         $geojson = $this->getGeojson();
         // MAPPING
-        $geojson['properties']['id'] = 'ec_track_' . $this->id;
+        $geojson['properties']['id'] = 'ec_track_'.$this->id;
         $geojson = $this->_mapElbrusGeojsonProperties($geojson);
 
         if ($this->ecPois) {
@@ -355,9 +356,9 @@ class EcTrack extends MultiLineString implements LayerRelatedModel
 
         $fields = ['kml', 'gpx'];
         foreach ($fields as $field) {
-            if (isset($geojson['properties'][$field . '_url'])) {
-                $geojson['properties'][$field] = $geojson['properties'][$field . '_url'];
-                unset($geojson['properties'][$field . '_url']);
+            if (isset($geojson['properties'][$field.'_url'])) {
+                $geojson['properties'][$field] = $geojson['properties'][$field.'_url'];
+                unset($geojson['properties'][$field.'_url']);
             }
         }
 
@@ -367,13 +368,13 @@ class EcTrack extends MultiLineString implements LayerRelatedModel
 
                 if ($taxonomy === 'activity') {
                     $geojson['properties']['taxonomy'][$name] = array_map(function ($item) use ($name) {
-                        return $name . '_' . $item;
+                        return $name.'_'.$item;
                     }, array_map(function ($item) {
                         return $item['id'];
                     }, $values));
                 } else {
                     $geojson['properties']['taxonomy'][$name] = array_map(function ($item) use ($name) {
-                        return $name . '_' . $item;
+                        return $name.'_'.$item;
                     }, $values);
                 }
             }
@@ -573,7 +574,7 @@ class EcTrack extends MultiLineString implements LayerRelatedModel
         }
 
         if (empty($searchables) || (in_array('name', $searchables) && ! empty($this->name))) {
-            $string .= str_replace('"', '', json_encode($this->getTranslations('name'))) . ' ';
+            $string .= str_replace('"', '', json_encode($this->getTranslations('name'))).' ';
         }
         // if (empty($searchables) || (in_array('description', $searchables) && ! empty($this->description))) {
         //     $description = str_replace('"', '', json_encode($this->getTranslations('description')));
@@ -586,16 +587,16 @@ class EcTrack extends MultiLineString implements LayerRelatedModel
         //     $string .= strip_tags($excerpt) . ' ';
         // }
         if (empty($searchables) || (in_array('ref', $searchables) && ! empty($this->ref))) {
-            $string .= $this->ref . ' ';
+            $string .= $this->ref.' ';
         }
         if (empty($searchables) || (in_array('osmid', $searchables) && ! empty($this->osmid))) {
 
-            $string .= $this->osmid . ' ';
+            $string .= $this->osmid.' ';
         }
 
         if (empty($searchables) || (in_array('taxonomyActivities', $searchables) && ! empty($this->taxonomyActivities))) {
             foreach ($this->taxonomyActivities as $tax) {
-                $string .= str_replace('"', '', json_encode($tax->getTranslations('name'))) . ' ';
+                $string .= str_replace('"', '', json_encode($tax->getTranslations('name'))).' ';
             }
         }
 
