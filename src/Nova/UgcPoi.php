6be192fba1e0301327c10304d3ea9a9150aa8f78
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-04-08 11:48:26 +0200

    feat: add ConvertUgcPoiToEcPoi action and integrate it into UgcPoi resource

--- src/Nova/UgcPoi.php
+++ src/Nova/UgcPoi.php
@@ -2,11 +2,20 @@
 
 namespace Wm\WmPackage\Nova;
 
+use Laravel\Nova\Http\Requests\NovaRequest;
 use Wm\WmPackage\Nova\Traits\PointResourceTrait;
+use Wm\WmPackage\Nova\Actions\ConvertUgcPoiToEcPoi;
 
 class UgcPoi extends AbstractUgcResource
 {
     use PointResourceTrait;
 
     public static $model = \Wm\WmPackage\Models\UgcPoi::class;
+
+    public function actions(NovaRequest $request): array
+    {
+        return [
+            new ConvertUgcPoiToEcPoi,
+        ];
+    }
 }
