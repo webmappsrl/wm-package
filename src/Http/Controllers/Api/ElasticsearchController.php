9a6e7f780de14c8be785f4cf239e2fcee682e2de
marco <m.baroncini@cyberway.cloud>
2025-04-16 16:47:28 +0200

    fix: es query

--- src/Http/Controllers/Api/ElasticsearchController.php
+++ src/Http/Controllers/Api/ElasticsearchController.php
@@ -2,12 +2,14 @@
 
 namespace Wm\WmPackage\Http\Controllers\Api;
 
+use Couchbase\RegexpSearchQuery;
 use Illuminate\Http\Request;
 use ONGR\ElasticsearchDSL\Aggregation\Bucketing\TermsAggregation;
 use ONGR\ElasticsearchDSL\Query\Compound\BoolQuery;
 use ONGR\ElasticsearchDSL\Query\FullText\MatchPhraseQuery;
 use ONGR\ElasticsearchDSL\Query\FullText\MatchQuery;
 use ONGR\ElasticsearchDSL\Query\TermLevel\RangeQuery;
+use ONGR\ElasticsearchDSL\Query\TermLevel\RegexpQuery;
 use ONGR\ElasticsearchDSL\Search;
 use Wm\WmPackage\Http\Controllers\Controller;
 use Wm\WmPackage\Models\EcTrack;
@@ -37,10 +39,17 @@ class ElasticsearchController extends Controller
         $appId = (int) last(explode('_', $app));
         $search = str_replace('%20', ' ', $query);
 
+        $search = explode(' ', $search);
+        # https://www.elastic.co/docs/reference/query-languages/query-dsl/query-dsl-query-string-query
+        $queryString = '';
+        foreach ($search as $word) {
+            $queryString .= "(name:$word OR name.edge:$word) AND ";
+        }
+        $queryString = rtrim($queryString, ' AND ');
+        //dd($queryString);
         // https://github.com/matchish/laravel-scout-elasticsearch?tab=readme-ov-file#conditions
-
         // base query
-        $query = EcTrack::search($search, function (\Elastic\Elasticsearch\Client $client, Search $body) use ($search, $layer) {
+        $query = EcTrack::search($queryString, function (\Elastic\Elasticsearch\Client $client, Search $body) use ($search, $layer) {
 
             // # The es driver for Laravel Scout
             // # https://github.com/matchish/laravel-scout-elasticsearch?tab=readme-ov-file#search
@@ -58,16 +67,23 @@ class ElasticsearchController extends Controller
             // Customize search to prioritize exact matches in name field
 
             $boolQuery = new BoolQuery;
-            $boolQuery->add(new MatchPhraseQuery('name.phrase', $search, [
-                'boost' => 10,
-            ]), BoolQuery::SHOULD); // #OR
-            $boolQuery->add(new MatchQuery('name.exact', $search, [
-                'boost' => 5,
-            ]), BoolQuery::SHOULD); // #OR
 
-            $boolQuery->add(new MatchQuery('name.edge', $search, [
-                'boost' => 4,
-            ]), BoolQuery::SHOULD); // #OR
+            // $boolQuery->add(new RegexpQuery('name.keyword', "$search.*", [
+            //     'case_insensitive' => true
+            // ]));
+
+
+
+            // $boolQuery->add(new MatchPhraseQuery('name.phrase', $search, [
+            //     'boost' => 10,
+            // ]), BoolQuery::SHOULD); // #OR
+            // $boolQuery->add(new MatchQuery('name.exact', $search, [
+            //     'boost' => 5,
+            // ]), BoolQuery::SHOULD); // #OR
+
+            // $boolQuery->add(new MatchQuery('name.edge', $search, [
+            //     'boost' => 4,
+            // ]), BoolQuery::SHOULD); // #OR
 
             if ($layer) {
                 $boolQuery->add(new \ONGR\ElasticsearchDSL\Query\TermLevel\TermsQuery('layers', [$layer]));
@@ -77,7 +93,7 @@ class ElasticsearchController extends Controller
             $body->addQuery($boolQuery);
 
             // # Dump the es query body as array
-            // dd($body->toArray());
+            //dd($body->toArray());
 
             // // Create a custom query that prioritizes exact matches
             // $customQuery = [
@@ -102,7 +118,7 @@ class ElasticsearchController extends Controller
             $body->addAggregation($activitiesAggregation);
             $body->addAggregation($themesAggregation);
 
-            // dd($body->toArray()); // #DEBUG
+            // dd($body->toArray()); // #DEBUG the whole es query body
 
             return $client->search(['index' => 'ec_tracks', 'body' => $body->toArray()])->asArray();
         })
@@ -132,6 +148,6 @@ class ElasticsearchController extends Controller
         }
 
         // results are formatted in wm-package/src/ElasticSearch/HitsIteratorAggregate.php
-        return $query->orderBy('name.keyword', 'asc')->take(10000)->get();
+        return collect($query->orderBy('name.keyword', 'asc')->take(10000)->get()['hits'])->pluck('name');
     }
 }
