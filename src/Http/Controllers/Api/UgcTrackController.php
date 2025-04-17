ecf0793969999a6c503c995f669be06f414eb39d
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-04-17 10:41:04 +0200

    osm2cai: added downloadGeojson method for UgcPoiController and UgcTrackController

--- src/Http/Controllers/Api/UgcTrackController.php
+++ src/Http/Controllers/Api/UgcTrackController.php
@@ -2,6 +2,7 @@
 
 namespace Wm\WmPackage\Http\Controllers\Api;
 
+use App\Models\UgcTrack as ModelsUgcTrack;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Wm\WmPackage\Http\Controllers\Api\Abstracts\UgcController;
@@ -28,5 +29,28 @@ class UgcTrackController extends UgcController
     public function destroy(UgcTrack $track): JsonResponse
     {
         return parent::_destroy($track);
+    }
+
+    //osm2cai api for geojson download
+    //TODO: update when osm2cai will use wm-package UGCs
+    public function downloadGeojson($ids)
+    {
+        $featureCollection = ['type' => 'FeatureCollection', 'features' => []];
+
+        $ids = explode(',', $ids);
+        $tracks = ModelsUgcTrack::whereIn('id', $ids)->get();
+
+        foreach ($tracks as $track) {
+            $feature = $track->getEmptyGeojson();
+            $feature['properties'] = $track->getJsonProperties();
+            $featureCollection['features'][] = $feature;
+        }
+
+        $headers = [
+            'Content-type' => 'application/json',
+            'Content-Disposition' => 'attachment; filename="ugc_tracks.geojson"',
+        ];
+
+        return response()->json($featureCollection, 200, $headers);
     }
 }
