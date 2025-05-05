69e6f778b1cb191c1f9b70a0d1a43bc9f683e02e
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-05-05 15:47:05 +0200

    feat: implement index query for user-specific resource access in AbstractEcResource

--- src/Nova/AbstractEcResource.php
+++ src/Nova/AbstractEcResource.php
@@ -2,12 +2,33 @@
 
 namespace Wm\WmPackage\Nova;
 
-use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
 use Wm\WmPackage\Nova\Filters\FeaturesByLayerFilter;
 
 abstract class AbstractEcResource extends AbstractGeometryResource
 {
+
+    /**
+     * Build an "index" query for the given resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        $user = Auth::user();
+
+        if ($user && !$user->hasRole('Administrator')) {
+            return $query->where('user_id', $user->id);
+        }
+
+        return $query;
+    }
+
+
     public function fields(NovaRequest $request): array
     {
         return [
