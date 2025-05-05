af533cf23e1ae2d05c2dd7198983cb76bedaccf5
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-05-05 10:27:59 +0200

    refactor: update EcPoiPolicy and EcTrackPolicy to simplify authorization logic and improve role handling

--- src/Policies/EcPoiPolicy.php
+++ src/Policies/EcPoiPolicy.php
@@ -13,93 +13,83 @@ class EcPoiPolicy
     /**
      * Perform pre-authorization checks.
      *
+     * @param  \App\Models\User  $user
      * @param  string  $ability
      * @return void|bool
      */
     public function before(User $user, $ability)
     {
-        // if ($user->hasRole('Admin')) {
-        //     return true;
-        // }
-        // if ($user->hasRole('Author') || $user->hasRole('Contributor')) {
-        //     return false;
-        // }
-
-        return true;
+        if ($user->hasRole('Administrator')) {
+            return true;
+        }
     }
 
     /**
      * Determine whether the user can view any models.
      *
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function viewAny(User $user)
     {
-        if ($user->hasRole('Editor')) {
-            return true;
-        }
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
      *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcPoi  $ecPoi
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function view(User $user, EcPoi $ecPoi)
     {
-        if ($user->hasRole('Editor') && $user->id === $ecPoi->user_id) {
-            return true;
-        }
-
-        return false;
+        // Admins are handled by before(). Users can view their own POIs.
+        return $user->id === $ecPoi->user_id;
     }
 
     /**
      * Determine whether the user can create models.
      *
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function create(User $user)
     {
-        if ($user->hasRole('Editor')) {
-            return true;
-        }
-
-        return false;
+        return true;
     }
 
     /**
      * Determine whether the user can update the model.
      *
+     * @param  \App\Models\User  $user
      * @param  \Wm\WmPackage\Models\EcPoi  $ecPoi
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function update(User $user, EcPoi $model)
+    public function update(User $user, EcPoi $ecPoi)
     {
-        if ($user->hasRole('Editor') && $user->id === $model->user_id) {
-            return true;
-        }
-
-        return false;
+        // Admins are handled by before(). Users can update their own POIs.
+        return $user->id === $ecPoi->user_id;
     }
 
     /**
      * Determine whether the user can delete the model.
      *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcPoi  $ecPoi
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function delete(User $user, EcPoi $ecPoi)
     {
-        if ($user->hasRole('Editor') && $user->id === $ecPoi->user_id) {
-            return true;
-        }
-
-        return false;
+        // Admins are handled by before(). Users can delete their own POIs.
+        return $user->id === $ecPoi->user_id;
     }
 
     /**
      * Determine whether the user can restore the model.
      *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcPoi  $ecPoi
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function restore(User $user, EcPoi $ecPoi)
@@ -110,6 +100,8 @@ class EcPoiPolicy
     /**
      * Determine whether the user can permanently delete the model.
      *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcPoi  $ecPoi
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function forceDelete(User $user, EcPoi $ecPoi)
--- src/Policies/EcTrackPolicy.php
+++ src/Policies/EcTrackPolicy.php
@@ -10,41 +10,101 @@ class EcTrackPolicy
 {
     use HandlesAuthorization;
 
-    public function viewAny(User $user): bool
+    /**
+     * Perform pre-authorization checks.
+     *
+     * @param  \App\Models\User  $user
+     * @param  string  $ability
+     * @return void|bool
+     */
+    public function before(User $user, $ability)
     {
-        return true;
-    }
-
-    public function view(User $user, EcTrack $model): bool
-    {
-        return true;
-    }
-
-    public function create(User $user): bool
-    {
-        return true;
-    }
-
-    public function update(User $user, EcTrack $model): bool
-    {
-        return true;
-    }
-
-    public function delete(User $user, EcTrack $model): bool
-    {
-        if ($user->hasRole('Editor')) {
+        if ($user->hasRole('Administrator')) {
             return true;
         }
-
-        return false;
     }
 
-    public function restore(User $user, EcTrack $model): bool
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcTrack  $ecTrack
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, EcTrack $ecTrack)
+    {
+        // Admins are handled by before(). Users can view their own Tracks.
+        return $user->id === $ecTrack->user_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcTrack  $ecTrack
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, EcTrack $ecTrack)
+    {
+        // Admins are handled by before(). Users can update their own Tracks.
+        return $user->id === $ecTrack->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcTrack  $ecTrack
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, EcTrack $ecTrack)
+    {
+        // Admins are handled by before(). Users can delete their own Tracks.
+        return $user->id === $ecTrack->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcTrack  $ecTrack
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, EcTrack $ecTrack)
     {
         return false;
     }
 
-    public function forceDelete(User $user, EcTrack $model): bool
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \Wm\WmPackage\Models\EcTrack  $ecTrack
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, EcTrack $ecTrack)
     {
         return false;
     }
