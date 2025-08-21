b0b46ba8cfdb799536e5dec8e5bb643689e9c63f
peppedeka <peppedeka@gmail.com>
2025-08-21 14:37:31 +0200

    chore(debug): 🔧 remove xdebug breakpoint from PropertiesPanel
    - Removed an unnecessary xdebug_break() call from the PropertiesPanel class to clean up the code.
    
    fix(url): 🐛 correct URL generation in TopUgcCreators
    
    - Updated URL generation to use the correct scheme and host in TopUgcCreators.
    - Ensured proper trimming of trailing slashes in the Nova path to prevent malformed URLs.

--- src/Nova/Metrics/TopUgcCreators.php
+++ src/Nova/Metrics/TopUgcCreators.php
@@ -107,7 +107,7 @@ class TopUgcCreators extends Table
         $resource = str_replace('_', '-', $ugcTable);
         $filter = base64_encode(json_encode([['resource:users:author' => $userId]]));
 
-        return url(Nova::path()."/resources/{$resource}?{$resource}_filter={$filter}");
+        return request()->getSchemeAndHttpHost() . rtrim(Nova::path(), '/') . "/resources/{$resource}?{$resource}_filter={$filter}";
     }
 
     /**
