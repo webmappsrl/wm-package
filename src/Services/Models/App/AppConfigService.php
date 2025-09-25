54da1849a24f8f88ad71adbf8ffb0e6c0b4aec31
Alessandro <alessandropeci@webmapp.it>
2025-09-25 13:06:00 +0200

    refactor(models): ♻️ update taxonomy identifiers management
    Enhanced the `getAllPoisGeojson` method in the `App` class to include additional taxonomy identifiers necessary for filtering. This involves merging several arrays of taxonomy identifiers with appropriate prefixes and adding them to the GeoJSON properties.
    
    Additionally, updated the `getAllPoiTaxonomies` method to streamline the retrieval of taxonomy data for POIs. Removed redundant code and optimized database queries to improve performance and maintainability. Addressed various TODOs related to missing data in the database and adjusted code comments accordingly.
    
    Also, modified access levels for certain methods in the `EcPoi` class to public to allow for broader usage and integration.
    
    Finally, ensured consistent import statements for exceptions across various services and cleaned up whitespace for better readability.

--- src/Services/Models/App/AppConfigService.php
+++ src/Services/Models/App/AppConfigService.php
@@ -298,7 +298,7 @@ class AppConfigService extends AppBaseService
                     } elseif (isset($item['geometry'])) {
                         $item['bbox'] = GeometryComputationService::make()->getGeometryModelBbox($layer);
                     }
-                } catch (\Exception $e) {
+                } catch (Exception $e) {
                     Log::warning('The bbox value '.$layer->id.' are not correct. Error: '.$e->getMessage());
                 }
                 // style
@@ -358,7 +358,7 @@ class AppConfigService extends AppBaseService
         $data['MAP']['pois']['poiIconRadius'] = $this->app->poi_icon_radius;
         $data['MAP']['pois']['poiMinZoom'] = $this->app->poi_min_zoom;
         $data['MAP']['pois']['poiLabelMinZoom'] = $this->app->poi_label_min_zoom;
-        // $data['MAP']['pois']['taxonomies'] = $this->app->getAllPoiTaxonomies(); //TODO trovare le tassonomie non piu tramite tema ma tramite associazione layer
+        $data['MAP']['pois']['taxonomies'] = $this->app->getAllPoiTaxonomies();
         $data['MAP']['pois']['poi_interaction'] = $this->app->poi_interaction;
 
         // Other Options
@@ -489,8 +489,8 @@ class AppConfigService extends AppBaseService
             $poi_types = [];
             try {
                 // TODO removed a.color and a.icon from taxonomy_poi_types because it does not exists in the database. Check if needed
-                $poi_types = DB::select("SELECT distinct a.id, a.identifier, a.name from taxonomy_poi_typeables as txa inner join ec_pois as t on t.id=txa.taxonomy_poi_typeable_id inner join taxonomy_poi_types as a on a.id=taxonomy_poi_type_id where txa.taxonomy_poi_typeable_type='App\Models\EcPoi' and t.user_id=$app_user_id ORDER BY a.name ASC;");
-            } catch (\Exception $e) {
+                $poi_types = DB::select("SELECT distinct a.id, a.identifier, a.name, a.icon from taxonomy_poi_typeables as txa inner join ec_pois as t on t.id=txa.taxonomy_poi_typeable_id inner join taxonomy_poi_types as a on a.id=taxonomy_poi_type_id where txa.taxonomy_poi_typeable_type='App\Models\EcPoi' and t.user_id=$app_user_id ORDER BY a.name ASC;");
+            } catch (Exception $e) {
                 Log::error('Error fetching poi types: '.$e->getMessage());
             }
 
@@ -499,7 +499,7 @@ class AppConfigService extends AppBaseService
                     'identifier' => 'poi_type_'.$poi_type->identifier,
                     'name' => json_decode($poi_type->name, true),
                     'id' => $poi_type->id,
-                    // 'icon' => $poi_type->icon,
+                    'icon_name' => $poi_type->icon,
                 ];
                 // if ($poi_type->color) {
                 //     $a['color'] = $poi_type->color;
