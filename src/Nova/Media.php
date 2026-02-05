c9fd054a7b25e42bf5c71b63bdf991ba288cfa03
peppedeka <peppedeka@users.noreply.github.com>
2026-02-05 11:27:36 +0000

    Fix styling

--- src/Nova/Media.php
+++ src/Nova/Media.php
@@ -32,7 +32,7 @@ class Media extends AbstractGeometryResource
 
         return [
             ...$this->fieldsFromTrait($request),
-            MorphTo::make(__('Model'),'model')
+            MorphTo::make(__('Model'), 'model')
                 ->types([
                     UgcPoi::class,
                     UgcTrack::class,
