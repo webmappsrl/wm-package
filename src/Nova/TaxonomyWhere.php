02a1cda9446c36c2931d9b02ffeb0e2864cde82a
peppedeka <peppedeka@gmail.com>
2026-03-25 12:05:21 +0100

    refactor(Nova): ♻️ remove unused Number field and add PropertiesPanel
    - Eliminated the unused `Number` use statement and corresponding field in the `TaxonomyWhere` class to clean up the code.
    - Introduced the `PropertiesPanel` to enhance the UI with a collapsible properties section.
    - Maintained existing functionality while improving code organization and readability.

--- src/Nova/TaxonomyWhere.php
+++ src/Nova/TaxonomyWhere.php
@@ -4,7 +4,6 @@ namespace Wm\WmPackage\Nova;
 
 use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Wm\WmPackage\Nova\Actions\CreateLayerFromTaxonomyWhere;
@@ -12,6 +11,7 @@ use Wm\WmPackage\Nova\Actions\ImportTaxonomyWhere;
 use Wm\WmPackage\Nova\Actions\RetryTaxonomyWhereGeometryFetch;
 use Wm\WmPackage\Nova\Actions\SyncTracksTaxonomyWhereAction;
 use Wm\WmPackage\Nova\Fields\FeatureCollectionMap\src\FeatureCollectionMap;
+use Wm\WmPackage\Nova\Fields\PropertiesPanel;
 use Wm\WmPackage\Nova\Filters\TaxonomyWhereAdminLevelFilter;
 use Wm\WmPackage\Nova\Filters\TaxonomyWhereHasGeometryFilter;
 use Wm\WmPackage\Nova\Filters\TaxonomyWhereSourceFilter;
@@ -42,8 +42,6 @@ class TaxonomyWhere extends AbstractTaxonomyResource
         return [
             ID::make()->sortable(),
             Text::make('name'),
-            Text::make('osmfeatures_id', fn () => $this->resource->getOsmfeaturesId())->readonly()->onlyOnDetail(),
-            Number::make('admin_level', fn () => $this->resource->getAdminLevel())->readonly()->onlyOnDetail(),
             Text::make('source', fn () => $this->resource->getSource())->readonly()->onlyOnIndex(),
             FeatureCollectionMap::make('Geometry', 'geometry')->onlyOnDetail(),
             Boolean::make('Geometry', function () {
@@ -52,6 +50,8 @@ class TaxonomyWhere extends AbstractTaxonomyResource
 
                 return ! is_null($model->geometry);
             })->onlyOnIndex(),
+            PropertiesPanel::makeWithModel(__('Proprietà'), 'properties', $this, false)
+                ->collapsible(),
         ];
     }
 
