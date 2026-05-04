65473514ee5c18091350349a387f288905d39a82
peppedeka <peppedeka@gmail.com>
2026-05-04 15:23:17 +0200

    fix(config): 🛠️ add visibility setting to wm-s3 filesystem
    - Added a 'visibility' setting with a value of 'public' to the 'wm-s3' filesystem configuration.
    - This change ensures that files stored are publicly accessible by default, aligning with expected access patterns.

--- config/wm-filesystems.php
+++ config/wm-filesystems.php
@@ -82,6 +82,7 @@ return [
             'url' => env('AWS_WMFE_URL', env('AWS_URL')),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
         ],
         's3-osfmedia' => [
             'driver' => 's3',
